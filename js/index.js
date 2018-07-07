f92cfa503ea11ff626a0c857faa528471d8bc6ab
Nico <nicoargiz@icloud.com>
2018-07-07 19:53:07 -0300

    Inicio formulario presupuesto

--- js/index.js
+++ js/index.js
@@ -14,21 +14,6 @@ function scrolling(event) {
 	);
 }
 
-
-//Scroll function Contacto
-var presupuestoButtonNode = $('#presupuestoButton')
-
-presupuestoButtonNode.click(scrolling2)
-
-function scrolling2(event) {
-	$('html, body').animate(
-		{
-			scrollTop: $('#contactSection').offset().top
-		},
-		1000
-	);
-}
-
 //Scroll function Nosotros
 var nosotrosButtonNode = $('#nosotrosButton')
 
@@ -38,6 +23,20 @@ function scrolling3(event) {
 	$('html, body').animate(
 		{
 			scrollTop: $('#nosotrosSection').offset().top
+		},
+		1000
+	);
+}
+
+//Scroll function Contacto
+var contactoButtonNode = $('#contactoButton')
+
+contactoButtonNode.click(scrolling4)
+
+function scrolling4(event) {
+	$('html, body').animate(
+		{
+			scrollTop: $('#contactSection').offset().top
 		},
 		1000
 	);
